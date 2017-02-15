created new plots, fixed the radians error that was giving me such a hard time

helix/sun_tracking/track_sun.py
@@ -1,11 +1,12 @@
 import numpy as np
 import ephem
 from math import degrees
+from math import radians
 
 # Expects an Nx5 table with mission day, lat, long, alt, (pressure?)
 flight_data = np.load("flight_data.npy")
 
-# mission day, lat, long, alt (km), (pressure?), horizon alt, azimuth
+# mission day, lat, long, alt (km), (pressure?), topocentric ra, dec
 sun_data = np.zeros((len(flight_data), len(flight_data[0]) + 2))
 
 start_date = ephem.date("2019/12/30")
@@ -13,10 +14,13 @@ sun = ephem.Sun()
 helix = ephem.Observer()
 
 for i in range(len(flight_data)):
+    idx = i                    # change to be stationary or i
     helix.date = start_date + flight_data[i,0]
-    helix.lat = flight_data[i,1]
-    helix.lon = flight_data[i,2]
-    helix.elevation = flight_data[i,3] * 1000 # convert to meters
+    # print(flight_data[idx,1])
+    helix.lat = radians(flight_data[idx,1])
+    helix.lon = radians(flight_data[idx,2])
+    # convert to meters:
+    helix.elevation = flight_data[idx,3] * 1000
     # Can also specify temperature or pressure, if that's what the last
     # column specifies
     sun.compute(helix)
@@ -25,10 +29,9 @@ for i in range(len(flight_data)):
     sun_data[i,5] = degrees(sun.alt)
     sun_data[i,6] = degrees(sun.az)
 
-print(sun_data)
-np.save("sun_data.npy", sun_data)
-np.savetxt("sun_data.csv", sun_data, delimiter=", ",
-           header="mission day, lat, long, alt, (pressure?), horizon alt, azimuth",
+np.save("sun_data_alts.npy", sun_data)
+np.savetxt("sun_data_alts.csv", sun_data, delimiter=", ",
+           header="mission day, lat, long, alt, (pressure?), horizon alts, azimuth",
            fmt=["%.3f", "%.4f", "%.3f", "%.4f", "%.3f", "%.5f", "%.5f"]
            )
 